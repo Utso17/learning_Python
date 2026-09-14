856a5fd0589dfc64eb92f63a7f969224cc8fb2bb
Utso17 <tanvirrezautso@gmail.com>
2026-09-14 13:17:57 +0600

    create a calculator with function and if/else

--- practice/calculator.py
+++ practice/calculator.py
@@ -1,3 +1,43 @@
-programming_languages = ('Rust', 'Java', 'Python', 'C++', 'Rust', 'Python')
-programming_languages.index('Python', 3)
-print(programming_languages)
+symbol = {
+    "add": "+",
+    "sub": "-",
+    "multiply": "*",
+    "division": "/"
+}
+choice = ["+", "-", "*", "/"]
+
+def input_check(input_num):
+    while True:
+        try:
+            return float(input(input_num))
+        except ValueError:
+            print("Input a number that is a digit!")
+
+num_1 = input_check("Enter the 1st Number: ")
+
+while True:
+    operations = input("What do you want to do? (+,-,*,/): ")
+    if operations not in choice:
+        print("Please choose a correct operation!")
+    else:
+        break
+
+num_2 = input_check("Enter the 2nd Number: ")
+
+# Calculations
+add = num_1 + num_2
+sub = num_1 - num_2
+multiply = num_1 * num_2
+
+if operations == "+":
+    print(f'{num_1} {operations} {num_2} = {add}')
+elif operations == "-":
+    print(f'{num_1} {operations} {num_2} = {sub}')
+elif operations == "*":
+    print(f'{num_1} {operations} {num_2} = {multiply}')
+else:
+    if num_2 == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        division = num_1 / num_2
+        print(f'{num_1} {operations} {num_2} = {division}')
